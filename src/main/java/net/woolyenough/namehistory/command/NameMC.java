d49475c49e572d813de79abef07468d493fbd5c8
Woolyenough <99221540+Woolyenough@users.noreply.github.com>
2022-02-09 18:15:13 +0000

    Slightly improved `/namemc` command

--- src/main/java/net/woolyenough/namehistory/command/NameMC.java
+++ src/main/java/net/woolyenough/namehistory/command/NameMC.java
@@ -5,8 +5,14 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import static net.minecraft.server.command.CommandManager.argument;
 
@@ -22,7 +28,10 @@ public class NameMC {
 
                             context.getSource().sendFeedback(new LiteralText("[Click] "+name+"'s NameMC").formatted(Formatting.BOLD).styled(
 
-                                style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://namemc.com/profile/"+ name))), false);
+                                    style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://namemc.com/profile/"+ name))
+                                            .withHoverEvent(new HoverEvent(
+                                                    HoverEvent.Action.SHOW_TEXT, new LiteralText("View on NameMC!"))
+                                            )), false);
 
                             return 0;
                         })));
