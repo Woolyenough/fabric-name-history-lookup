9fc5edfc0f88e088c210c198573545029cf38bdf
Woolyenough <99221540+Woolyenough@users.noreply.github.com>
2022-02-11 11:52:19 +0000

    Update ClientCommands.java

--- src/main/java/net/woolyenough/namehistory/ClientCommands.java
+++ src/main/java/net/woolyenough/namehistory/ClientCommands.java
@@ -10,15 +10,13 @@ import net.minecraft.text.HoverEvent;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
 
-/*
-import org.json.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+//import org.json.*;
+
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-*/
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Environment(EnvType.CLIENT)
 public final class ClientCommands implements ClientModInitializer {
@@ -34,67 +32,101 @@ public final class ClientCommands implements ClientModInitializer {
             return 0;
         }));
 */
-        ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("namemc")
-                .then(ClientCommandManager.argument("Player Name", StringArgumentType.string())
-                        .executes(context -> {
 
-                            String name = String.valueOf(StringArgumentType.getString(context, "Player Name"));
-
-                            context.getSource().sendFeedback(new LiteralText("[Click] " + name + "'s NameMC").formatted(Formatting.BOLD).styled(
-
-                                    style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://namemc.com/profile/" + name))
-                                            .withHoverEvent(new HoverEvent(
-                                                    HoverEvent.Action.SHOW_TEXT, new LiteralText("View on NameMC!"))
-                                            )));
-
-                            return 0;
-                        })));
-/*
         ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("lookup")
                 .then(ClientCommandManager.argument("Player Name", StringArgumentType.string())
                         .executes(context -> {
+
                             String name = String.valueOf(StringArgumentType.getString(context, "Player Name"));
 
-                            context.getSource().sendFeedback(new LiteralText("The person is not in your game"));
-                            String resp = null;
-
                             try {
-
-                                resp = getRequest("https://api.mojang.com/users/profiles/minecraft/" + name);
-
-                            } catch (IOException e) {
-                                context.getSource().sendFeedback(new LiteralText("An error occurred when trying " +
-                                        "to connect to https://api.mojang.com/users/profiles/minecraft/" + name));
+                                String uuid = get_api_request("https://api.mojang.com/users/profiles/minecraft/" + name).split("\"")[7];
+                                List<String> nameHistory = new ArrayList<String>(Arrays.asList(get_api_request("https://api.mojang.com/user/profiles/" + uuid + "/names").split("\\[|\\]|\\{|\\}|\"|\\,")));
+                                nameHistory.removeAll(Arrays.asList("", null, " ", ":"));
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                context.getSource().sendError(new LiteralText("No account exists by that name .-."));
+                                return 0;
                             }
-                            String jsonString = resp;
-                            JSONArray obj = new JSONArray(jsonString);
-                            try {
-                                String accountName = obj.getJSONObject(0).getString("name");
-                                String accountUUID = obj.getJSONObject(0).getString("id");
-                                context.getSource().sendFeedback(new LiteralText("UUID: " + accountUUID + "\nName: " + accountName+"\nNameMC: https://namemc.com/profile/"+accountUUID));
-                            }catch(Exception e){context.getSource().sendError(new LiteralText("That user does not exist!"));}
 
+                            //context.getSource().sendFeedback(new LiteralText(nameHistory.toString()));
+
+                            context.getSource().sendFeedback(new LiteralText("[Hover] View name history").formatted(Formatting.BOLD).styled(
+
+                                    style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://namemc.com/profile/" + name))
+                                            .withHoverEvent(new HoverEvent(
+                                                    HoverEvent.Action.SHOW_TEXT, new LiteralText(get_name_history(name) + "\n\n§rClick to open NameMC!"))
+                                            )));
                             return 0;
                         })));
-
-
     }
-    private static String getRequest(String urlToRead) throws IOException {
-        URL url = new URL(urlToRead);
-        URLConnection connection = url.openConnection();
 
-        ArrayList<String> stuff = new ArrayList<String>();
+    private String get_name_history(String name) {
+        String uuid = get_api_request("https://api.mojang.com/users/profiles/minecraft/" + name).split("\"")[7];
+        List<String> nameHistory = new ArrayList<String>(Arrays.asList(get_api_request("https://api.mojang.com/user/profiles/" + uuid + "/names").split("\\[|\\]|\\{|\\}|\"|\\,")));
+        nameHistory.removeAll(Arrays.asList("", null, " ", ":"));
+        StringBuilder output = new StringBuilder();
 
-        try (BufferedReader in = new BufferedReader(
-                new InputStreamReader(connection.getInputStream())))
-        {
-            String line;
-            while ((line = in.readLine()) != null) {
-                stuff.add(line);
+        for (int i = 0; i < nameHistory.size(); i++) {
+            switch (nameHistory.get(i)) {
+                case "name": {
+                    i++;
+
+                    if (nameHistory.get(i).toLowerCase().equals(name)){
+                        if (i == 1)
+                            output.append("§r§a§l");
+                        else
+                            output.append("\n§r§a§l");
+                    }
+                    else{
+                        if (i == 1)
+                            output.append("§r§b");
+                        else
+                            output.append("\n§r§b");
+                    }
+
+                    output.append(nameHistory.get(i));
+                    break;
+                }
+                case "changedToAt": {
+                    i++;
+                    DateFormat simple = new SimpleDateFormat("dd MMM yyyy (HH:mm)");
+                    Date result = new Date(Long.parseLong(nameHistory.get(i).split(":")[1]));
+                    output.append("  §r§7").append(simple.format(result));
+                    break;
+                }
             }
         }
-        return stuff.toString();
+        return output.toString();
     }
-*/
+
+    private String get_api_request(String api_url) {
+        String output = "";
+        try {
+            URL url = new URL(api_url);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            //Getting the response code
+            int response = conn.getResponseCode();
+
+            if (response != 200) {;} else {
+                StringBuilder inline = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+
+                //Write all the JSON data into a string using a scanner
+                while (scanner.hasNext()) {
+                    inline.append(scanner.nextLine());
+                }
+
+                //Close the scanner
+                scanner.close();
+                output = inline.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return output;
     }
 }
